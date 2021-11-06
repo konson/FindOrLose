e15e4dff41d0130b1ba14a203dd839d76585bd13
alecs <alecs429@gmail.com>
2021-11-06 11:55:41 -0400

    Add notes, convert to dataTaskPublisher

--- FindOrLose/Models/RandomImageResponse.swift
+++ FindOrLose/Models/RandomImageResponse.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,3 +31,107 @@ import Foundation
 struct RandomImageResponse: Decodable {
   let urls: ImageType
 }
+
+// MARK: - Sample of complete JSON returned from API.
+
+//// 20211106111706
+//// https://api.unsplash.com/photos/random/?client_id=MkvxqMCKT5I9jCw1U325uEFET0c85d43vr7eJxsaDg4
+//
+//{
+//  "id": "NFPw8HFsLg0",
+//  "created_at": "2021-10-13T21:20:33-04:00",
+//  "updated_at": "2021-11-06T04:30:57-04:00",
+//  "promoted_at": "2021-10-14T17:40:01-04:00",
+//  "width": 4160,
+//  "height": 6240,
+//  "color": "#c0d9d9",
+//  "blur_hash": "LgK23HoLi^$%~qWAxFoJx]t6ofbH",
+//  "description": null,
+//  "alt_description": null,
+//  "urls": {
+//    "raw": "https://images.unsplash.com/photo-1634173987034-bab553cdf219?ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU&ixlib=rb-1.2.1",
+//    "full": "https://images.unsplash.com/photo-1634173987034-bab553cdf219?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU&ixlib=rb-1.2.1&q=85",
+//    "regular": "https://images.unsplash.com/photo-1634173987034-bab553cdf219?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU&ixlib=rb-1.2.1&q=80&w=1080",
+//    "small": "https://images.unsplash.com/photo-1634173987034-bab553cdf219?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU&ixlib=rb-1.2.1&q=80&w=400",
+//    "thumb": "https://images.unsplash.com/photo-1634173987034-bab553cdf219?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU&ixlib=rb-1.2.1&q=80&w=200"
+//  },
+//  "links": {
+//    "self": "https://api.unsplash.com/photos/NFPw8HFsLg0",
+//    "html": "https://unsplash.com/photos/NFPw8HFsLg0",
+//    "download": "https://unsplash.com/photos/NFPw8HFsLg0/download?ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU",
+//    "download_location": "https://api.unsplash.com/photos/NFPw8HFsLg0/download?ixid=MnwyNjkzODF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzYyMTE4MjU"
+//  },
+//  "categories": [
+//
+//  ],
+//  "likes": 48,
+//  "liked_by_user": false,
+//  "current_user_collections": [
+//
+//  ],
+//  "sponsorship": null,
+//  "topic_submissions": {
+//    "street-photography": {
+//      "status": "rejected"
+//    }
+//  },
+//  "user": {
+//    "id": "RSJOaCR98Os",
+//    "updated_at": "2021-11-06T09:30:48-04:00",
+//    "username": "duongtrungthinh",
+//    "name": "Duong Thinh",
+//    "first_name": "Duong",
+//    "last_name": "Thinh",
+//    "twitter_username": null,
+//    "portfolio_url": "https://www.flickr.com/photos/trungthinhduong0303/",
+//    "bio": null,
+//    "location": "Osaka Japan",
+//    "links": {
+//      "self": "https://api.unsplash.com/users/duongtrungthinh",
+//      "html": "https://unsplash.com/@duongtrungthinh",
+//      "photos": "https://api.unsplash.com/users/duongtrungthinh/photos",
+//      "likes": "https://api.unsplash.com/users/duongtrungthinh/likes",
+//      "portfolio": "https://api.unsplash.com/users/duongtrungthinh/portfolio",
+//      "following": "https://api.unsplash.com/users/duongtrungthinh/following",
+//      "followers": "https://api.unsplash.com/users/duongtrungthinh/followers"
+//    },
+//    "profile_image": {
+//      "small": "https://images.unsplash.com/profile-1623978803275-e8bfe25486d7image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+//      "medium": "https://images.unsplash.com/profile-1623978803275-e8bfe25486d7image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+//      "large": "https://images.unsplash.com/profile-1623978803275-e8bfe25486d7image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+//    },
+//    "instagram_username": "trungthinhduong",
+//    "total_collections": 0,
+//    "total_likes": 0,
+//    "total_photos": 74,
+//    "accepted_tos": true,
+//    "for_hire": true,
+//    "social": {
+//      "instagram_username": "trungthinhduong",
+//      "portfolio_url": "https://www.flickr.com/photos/trungthinhduong0303/",
+//      "twitter_username": null,
+//      "paypal_email": null
+//    }
+//  },
+//  "exif": {
+//    "make": "Canon",
+//    "model": "Canon EOS 6D Mark II",
+//    "name": "Canon, EOS 6D Mark II",
+//    "exposure_time": "1/800",
+//    "aperture": "11.0",
+//    "focal_length": "100.0",
+//    "iso": 1250
+//  },
+//  "location": {
+//    "title": null,
+//    "name": null,
+//    "city": null,
+//    "country": null,
+//    "position": {
+//      "latitude": null,
+//      "longitude": null
+//    }
+//  },
+//  "views": 182019,
+//  "downloads": 966
+//}
--- FindOrLose/Network/UnsplashAPI.swift
+++ FindOrLose/Network/UnsplashAPI.swift
@@ -27,11 +27,51 @@
 /// THE SOFTWARE.
 
 import Foundation
+import Combine
 
 enum UnsplashAPI {
-  static let accessToken = ""
+  static let accessToken = "MkvxqMCKT5I9jCw1U325uEFET0c85d43vr7eJxsaDg4"
 
-  static func randomImage(completion: @escaping (RandomImageResponse?) -> Void) {
+  /// Replace with new signature. Now, the method doesn’t take a completion closure as a parameter. Instead, it returns a publisher, with an output type of RandomImageResponse and a failure type of GameError.
+  static func randomImage() -> AnyPublisher<RandomImageResponse, GameError> {
+    let url = URL(string: "https://api.unsplash.com/photos/random/?client_id=\(accessToken)")!
+
+    let config = URLSessionConfiguration.default
+    config.requestCachePolicy = .reloadIgnoringLocalCacheData
+    config.urlCache = nil
+    let session = URLSession(configuration: config)
+
+    var urlRequest = URLRequest(url: url)
+    urlRequest.addValue("Accept-Version", forHTTPHeaderField: "v1")
+    
+/// 1. You get a publisher from the URL session for your URL request. This is a URLSession.DataTaskPublisher, which has an output type of (data: Data, response: URLResponse). That’s not the right output type, so you’re going to use a series of operators to get to where you need to be.
+    return session.dataTaskPublisher(for: urlRequest)
+    
+    /// 2. Apply the tryMap operator. This operator takes the upstream value and attempts to convert it to a different type, with the possibility of throwing an error. There is also a map operator for mapping operations that can’t throw errors.
+      .tryMap { response in
+        guard
+          let httpURLResponse = response.response as? HTTPURLResponse,
+          /// 3. Check for 200 OK HTTP status.
+          httpURLResponse.statusCode == 200
+        else {
+          /// 4. Throw the custom GameError.statusCode error if you did not get a 200 OK HTTP status.
+          throw GameError.statusCode
+        }
+        /// 5. Return the response.data if everything is OK. This means the output type of your chain is now Data
+        return response.data
+      }
+    
+      /// 6. Apply the decode operator, which will attempt to create a RandomImageResponse from the upstream value using JSONDecoder. Your output type is now correct!
+      .decode(type: RandomImageResponse.self, decoder: JSONDecoder())
+    
+      /// 7. Your failure type still isn’t quite right. If there was an error during decoding, it won’t be a GameError. The mapError operator lets you deal with and map any errors to your preferred error type, using the function you added to GameError.
+      .mapError { GameError.map($0) }
+    
+    /// 8. If you were to check the return type of mapError at this point, you would be greeted with something quite horrific. The .eraseToAnyPublisher operator tidies all that mess up so you’re returning something more usable.
+      .eraseToAnyPublisher()
+  }
+    
+  static func randomImageOLD(completion: @escaping (RandomImageResponse?) -> Void) {
     let url = URL(string: "https://api.unsplash.com/photos/random/?client_id=\(accessToken)")!
 
     let config = URLSessionConfiguration.default
